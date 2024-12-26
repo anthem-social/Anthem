d5b4ba2a4fb49317c83fafa75b72b62d9e432921
Avery Schreiner <avery.schreiner@archerirm.com>
2024-12-26 16:45:07 -0600

    profile beginnings

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,11 @@
-import { Image, StyleSheet } from 'react-native';
+import { Dimensions, Image, StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import AnthemView from '@/components/AnthemView';
-import { Track, User } from '@/types';
+import { Status, Track, User } from '@/types';
 import { ThemedView } from '@/components/ThemedView';
+import ScrollingTrack from '@/components/ScrolligTrack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { playUri } from '@/api/spotify';
 
 const anthem: Track = {
   uri: 'spotify:track:3KkXRkHbMCARz0aVfEt68P',
@@ -23,11 +26,13 @@ const anthem: Track = {
   }
 }
 
-const mock: User = {
+// Limit to 110 chars
+// Change alias to nickname
+const mockUser: User = {
   id: 'schreineravery-us',
   alias: 'Avery',
-  pictureUrl: 'https://i.scdn.co/image/ab67616d0000b273e2e352d89826aef6dbd5ff8f',
   bio: 'the creator',
+  pictureUrl: 'https://i.scdn.co/image/ab67616d0000b273e2e352d89826aef6dbd5ff8f',
   anthem: anthem,
   joined: new Date(),
   followers: ['schreineravery-us'],
@@ -35,20 +40,58 @@ const mock: User = {
   friends: ['schreineravery-us'],
 }
 
-type Props = {
-  user: User;
-  isCurrentUser: boolean;
+const mockStatus: Status = {
+  userId: 'schreineravery-us',
+  track: anthem,
+  lastChanged: new Date()
 }
 
-export default function Profile({ user = mock, isCurrentUser }: Props) {
+const play = async (uri: string) => {
+    await playUri(uri);
+}
 
+export type Props = {
+  status: Status,
+  user: User,
+  isCurrentUser: boolean
+}
+
+export default function Profile({ status = mockStatus, user = mockUser, isCurrentUser = true}: Props) {
   return (
     <AnthemView>
+      <ThemedView style={[styles.row, { alignItems: 'center', justifyContent: 'space-between' }]}>
+        <Icon name="more-horiz" size={30} color={'transparent'} />
+        <ThemedText style={styles.alias}>
+          {user.alias}
+        </ThemedText>
+        <Icon name="more-horiz" size={30} color={'grey'} />
+      </ThemedView>
       <ThemedView style={styles.row}>
         <Image source={{ uri: user.pictureUrl }} style={styles.picture} />
+      </ThemedView>
+      <ThemedView style={styles.row}>
+        <ThemedText style={styles.bio}>
+          {user.bio}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.row}>
+        <ThemedView style={[styles.row, { marginBottom: 0, maxWidth: Dimensions.get('window').width - 50 }]}>
+          <ScrollingTrack {...status.track} />
+          <Icon name="play-arrow" size={24} color={'grey'} onPress={() => play(status.track.uri)}/>
+        </ThemedView>
+      </ThemedView>
+      <ThemedView style={styles.row}>
         <ThemedView style={styles.col}>
-          <ThemedText style={styles.alias}>{user.alias}</ThemedText>
-          <ThemedText style={styles.bio}>{user.bio}</ThemedText>
+          <ThemedText>Active</ThemedText>
+          <ThemedText>15m ago</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.col}>
+          <ThemedText>Followers</ThemedText>
+          <ThemedText>{user.followers.length}</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.col}>
+          <ThemedText>Following</ThemedText>
+          <ThemedText>{user.following.length}</ThemedText>
         </ThemedView>
       </ThemedView>
     </AnthemView>
@@ -57,49 +100,33 @@ export default function Profile({ user = mock, isCurrentUser }: Props) {
 
 const styles = StyleSheet.create({
   alias: {
-    fontSize: 24,
+    fontSize: 26,
     fontWeight: 'bold',
     paddingTop: 8,
-  },
-  bio: {
-    fontSize: 12,
-    color: 'grey'
-  },
-  col: {
-    flexDirection: 'column',
-    gap: 2
-  },
-  container: {
+    textAlign: 'center',
     flex: 1
   },
-  header: {
-    borderBottomWidth: 12,
-    borderBottomColor: 'red',
-    borderStyle: 'solid',
+  bio: {
+    fontSize: 14
   },
-  joined: {
-    fontSize: 12,
-    color: 'grey',
+  col: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center'
   },
+  // menu: {
+  //   marginLeft: 'auto'
+  // },
   picture: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    width: 140,
+    height: 140,
+    borderRadius: 70,
     borderColor: 'grey',
     borderWidth: 1,
   },
   row: {
     flexDirection: 'row',
-    gap: 28
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 16,
-    marginBottom: 8,
-  },
-  userId: {
-    fontSize: 16,
-    marginBottom: 16,
-  },
+    justifyContent: 'center',
+    marginBottom: 10,
+  }
 });
