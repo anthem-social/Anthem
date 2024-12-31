9f4369974d5f59b11d5737a963fee068bba74203
Avery Schreiner <avery.schreiner@archerirm.com>
2024-12-30 20:08:28 -0600

    init search page

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import AnthemView from '@/components/AnthemView';
+import { ThemedText } from '@/components/ThemedText';
+
+export default function Search() {
+  
+  return (
+    <AnthemView>
+        <ThemedText>This will be the search page.</ThemedText>
+    </AnthemView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
