2af783fcc8b907d1d64228fa94044f6eb513119b
Avery Schreiner <avery.schreiner@archerirm.com>
2025-01-04 18:33:42 -0600

    add play button

--- components/Core/TrackCard.tsx
+++ components/Core/TrackCard.tsx
@@ -3,21 +3,26 @@ import { Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Track } from '@/types';
 import { Linking } from 'react-native';
 import { Icon, Text, View } from '@/components/Themed';
+import { playUri } from '@/api/spotify';
 
 export function TrackCard(track: Track) {
   const open = async (uri: string) => {
     await Linking.openURL(uri);
   };
 
+  const play = async (uri: string) => {
+      await playUri(uri);
+  }
+
   return (
-    <View style={[styles.row, { gap: 4}]}>
+    <View style={styles.container}>
       <TouchableOpacity onPress={() => open(track.album.uri)}>
         <Image source={{ uri: track.album.coverUrl }} style={styles.cover} />
       </TouchableOpacity>
-      <View style={styles.col}>
+      <View style={[styles.col, { flex: 1 }]}>
         <View style={styles.row}>
           <Icon family="Ionicons" name="disc-outline" size={16} style={styles.icon} />
-          <ScrollView style={{ width: " 78%" }} horizontal showsHorizontalScrollIndicator={false} scrollEnabled={true}>
+          <ScrollView style={styles.scroll} horizontal showsHorizontalScrollIndicator={false} scrollEnabled={true}>
             <Text style={styles.text} onPress={() => open(track.uri)}>
               {track.name}
             </Text>
@@ -25,7 +30,7 @@ export function TrackCard(track: Track) {
         </View>
         <View style={styles.row}>
           <Icon family="Ionicons" name="person-circle-outline" size={16} style={styles.icon} />
-          <ScrollView style={{ width: "78%" }} horizontal showsHorizontalScrollIndicator={false} scrollEnabled={true}>
+          <ScrollView style={styles.scroll} horizontal showsHorizontalScrollIndicator={false} scrollEnabled={true}>
             {track.artists.map((artist, index) => (
               <Text key={index} style={styles.text} onPress={() => open(artist.uri)} numberOfLines={1}>
                   {artist.name}{index < track.artists.length - 1 ? ', ' : ''}
@@ -33,6 +38,9 @@ export function TrackCard(track: Track) {
             ))}
           </ScrollView>
         </View>
+      </View>
+      <View style={styles.col}>
+        <Icon family="Ionicons" name="play-circle-outline" size={32} onPress={() => play(track.uri)}/>
       </View>
     </View>
   );
@@ -42,6 +50,14 @@ const styles = StyleSheet.create({
   col: {
     flexDirection: 'column',
     justifyContent: 'center',
+    alignContent: 'center',
+    gap: 4
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    flexDirection: 'row',
     gap: 4
   },
   cover: {
@@ -55,6 +71,9 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row'
+  },
+  scroll: {
+    width: "68%"
   },
   text: {
     fontSize: 14
