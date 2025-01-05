08fb9f6c229ca9636cbf99e95887dfe18a2ac794
Avery Schreiner <avery.schreiner@archerirm.com>
2025-01-04 18:33:51 -0600

    add octicons

--- components/Themed/Icon.tsx
+++ components/Themed/Icon.tsx
@@ -1,6 +1,7 @@
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import Octicons from 'react-native-vector-icons/Octicons';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
 
@@ -28,6 +29,9 @@ export function Icon({ lightColor, darkColor, type = 'default', ...rest }: Props
       break;
     case 'MaterialIcons':
       Icon = MaterialIcon;
+      break;
+    case 'Octicons':
+      Icon = Octicons;
       break;
     default:
       Icon = Ionicon;
