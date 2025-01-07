dd1109cbaee8fcfd0cbb5bfb0cacf0c2e9a4aa7a
Avery Schreiner <avery.schreiner@archerirm.com>
2025-01-06 23:18:42 -0600

    welcome page imporovements init

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,26 +1,58 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Linking, StyleSheet } from "react-native";
 import { View, Text, Button } from "@/components/Themed";
+import { AnthemView } from "@/components/Core";
 
 interface WelcomeProps {
   onPress: () => void;
 }
 
 const WelcomeScreen: React.FC<WelcomeProps> = ({ onPress }) => {
+  const [spotifyIsInstalled, setSpotifyInstalled] = useState(false);
+  const [spotifyIsOpen, setSpotifyIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function gatherInfo() {
+      const isInstalled = await Linking.canOpenURL("spotify://");
+      console.log("isInstalled", isInstalled);
+      setSpotifyInstalled(isInstalled);
+
+      if (!isInstalled) {
+        return;
+      }
+
+      const initialUrl = await Linking.getInitialURL();
+      console.log("initialUrl", initialUrl);
+
+      if (initialUrl == null) {
+        return;
+      }
+
+      const isOpen = initialUrl.includes("spotify:");
+      console.log("isOpen", isOpen);
+      setSpotifyIsOpen(isOpen);
+    }
+
+    gatherInfo();
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <AnthemView>
+      <View style={styles.container}>
         <Text>Welcom to Anthem!</Text>
         <Button title="Connect your Spotify account to continue" onPress={onPress} />
-    </View>
+      </View>
+    </AnthemView>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-  });
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 100
+  }
+});
   
 export default WelcomeScreen;
